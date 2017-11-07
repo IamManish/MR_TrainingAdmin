60172ba7d5914704819c010251aab9344e549db0
Manish Ranjan <manish.ranjan@netsmartz.local>
2017-11-07 14:51:34 +0530

    Added files

--- app/code/local/MR/TrainingAdmin/controllers/Adminhtml/TrainingadminbackendController.php
+++ app/code/local/MR/TrainingAdmin/controllers/Adminhtml/TrainingadminbackendController.php
@@ -1,17 +1,16 @@
 <?php
-class MR_TrainingAdmin_Adminhtml_TrainingadminbackendController extends Mage_Adminhtml_Controller_Action
-{
 
-	protected function _isAllowed()
-	{
-		//return Mage::getSingleton('admin/session')->isAllowed('trainingadmin/trainingadminbackend');
-		return true;
-	}
+class MR_TrainingAdmin_Adminhtml_TrainingadminbackendController extends Mage_Adminhtml_Controller_Action {
 
-	public function indexAction()
-    {
-       $this->loadLayout();
-	   $this->_title($this->__("Adminhtml Training Page"));
-	   $this->renderLayout();
+    protected function _isAllowed() {
+        return Mage::getSingleton('admin/session')->isAllowed('trainingadmin/trainingadminbackend');
+        //return true;
     }
+
+    public function indexAction() {
+        $this->loadLayout();
+        $this->_title($this->__("Adminhtml Training Page"));
+        $this->renderLayout();
+    }
+
 }
